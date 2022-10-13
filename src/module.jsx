30a17e1b88f8476d68a79845c8f0ee5138bd6265
anna <unursey@gmail.com>
2022-10-14 01:43:12 +0300

    first commit

---
+++ src/module.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import cat from './img/123.png';
+
+const Module = () => (
+    <>
+        <h1>Hello, React!</h1>
+        <img src={cat} />
+    </>
+);
+
+export default Module;
